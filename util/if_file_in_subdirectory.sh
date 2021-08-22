a35cc15777a705967084f7acc774dcdca71d998a
Sam Kerridge <sam.kerridge@xero.com>
2021-08-23 11:22:15 +1200

    Updating subdirectory script
    - Now has multi-folder support for matching a given number of folders
    - This allows us to watch important build folders

--- util/if_file_in_subdirectory.sh
+++ util/if_file_in_subdirectory.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-search_dir=$1
-
 echo "=============== list modified files ==============="
 git diff --name-only HEAD^ HEAD
 
 echo "========== check paths of modified files =========="
 git diff --name-only HEAD^ HEAD > files.txt
-while IFS= read -r file
-do
-    echo $file
-    if [[ $file == $search_dir/** ]]; then
-        echo "Found $file in the '$search_dir' directory"
-        echo "::set-output name=run_job::true"
-        break
-    else
-        echo "::set-output name=run_job::false"
-    fi
-done < files.txt
+
+# takes multiple directories as input
+for search_dir in "$@"; do
+    # check all modified files
+    while IFS= read -r file; do
+        echo $file
+        if [[ $file == $search_dir/** ]]; then
+            echo "Found $file in the '$search_dir' directory"
+            echo "Triggering build"
+            echo "::set-output name=run_job::true"
+            break 2
+        else
+            echo "::set-output name=run_job::false"
+        fi
+    done < files.txt
+done
