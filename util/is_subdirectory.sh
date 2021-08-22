31ef93a25faa278ac41e094b340fe0ca3514b4b9
Sam Kerridge <sam.kerridge@xero.com>
2021-08-23 01:06:24 +1200

    Github actions trigger

---
+++ util/is_subdirectory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "=============== list modified files ==============="
+git diff --name-only HEAD^ HEAD
+
+echo "========== check paths of modified files =========="
+git diff --name-only HEAD^ HEAD > files.txt
+while IFS= read -r file
+do
+    echo $file`
+    if [[ $file != $1/* ]]; then
+        echo "This modified file is not under the '$1' folder."
+        echo "::set-output name=run_job::false"
+        break
+    else
+        echo "::set-output name=run_job::true"
+    fi
+done < files.txt
