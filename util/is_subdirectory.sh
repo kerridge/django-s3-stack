50c9640f1b6565540b14fb9f11f78be700f5a492
Sam Kerridge <sam.kerridge@xero.com>
2021-08-23 01:38:13 +1200

    agaaaaain

--- util/is_subdirectory.sh
+++ util/is_subdirectory.sh
@@ -9,15 +9,22 @@ echo "========== check paths of modified files =========="
 git diff --name-only HEAD^ HEAD > files.txt
 while IFS= read -r file
 do
-    pwd
     echo $file
     if [[ $(find "$search_dir" -name "$file") ]]; then
-        echo "This modified file IS under the '$1' folder."
-        echo "::set-output name=run_job::true"
-        break
-    else
         echo "This modified file is not under the '$1' folder."
         echo "::set-output name=run_job::false"
         break
+    else
+        echo "This modified file IS under the '$1' folder."
+        echo "::set-output name=run_job::true"
+        break
     fi
 done < files.txt
+
+# FILE=../api/home/views.py
+# filename=views.py
+# search_dir=api/
+# cd ../
+# if [[ $(find "$search_dir" -name "$filename") ]]; then
+#     echo "$filename exists."
+# fi
