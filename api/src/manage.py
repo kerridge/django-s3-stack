092a23bc6f34aa55d03a1858095023d4f153f17c
Sam Kerridge <sam.kerridge@xero.com>
2021-08-23 17:18:47 +1200

    Added debug configuration for django project

--- api/src/manage.py
+++ api/src/manage.py
@@ -5,6 +5,18 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "headless_test.settings.dev")
 
+    from django.conf import settings
+
+    
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import ptvsd
+
+            ptvsd.enable_attach(address=('0.0.0.0', 3000))
+            print('Waiting for debugger to attach, try launching `Run Django` debug configuration')
+            ptvsd.wait_for_attach()
+            print('Attached!')
