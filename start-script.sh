4663221154796e5b6feb5d0356d7313f2c0b5ec0
Sam Kerridge <sam.kerridge@xero.com>
2021-08-29 16:24:16 +1200

    Changing over to start-script

--- start-script.sh
+++ start-script.sh
@@ -1,5 +1,12 @@
 # !/bin/sh
 
+# Github access token with read/write access to ssh keys
+GH_SSH_ACCESS_TOKEN = <GH_ACCESS_TOKEN>
+# SSH URL from the repository to clone
+GH_SSH_URL = <GH_SSH_URL>
+# Github Username
+GH_USERNAME = <GH_USERNAME>
+
 # Install git
 yum install git
 
@@ -18,31 +25,31 @@ firewall-cmd --list-all | grep http
 # https://www.thegeekdiary.com/how-to-open-a-ports-in-centos-rhel-7/
 # lsof -i -P |grep http
 
-# Public Key generated on personal machine
-REMOTE_PUBLIC_KEY="" # Matching Private Key should be stored in Github Secrets
+# # Public Key generated on personal machine
+# REMOTE_PUBLIC_KEY="" # Matching Private Key should be stored in Github Secrets
 
-# Add public key to allow SSH access to this machine from Github Actions
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
-echo ssh-rsa $REMOTE_PUBLIC_KEY > /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+# # Add public key to allow SSH access to this machine from Github Actions
+# mkdir -p /root/.ssh
+# chmod 700 /root/.ssh
+# echo ssh-rsa $REMOTE_PUBLIC_KEY > /root/.ssh/authorized_keys
+# chmod 600 /root/.ssh/authorized_keys
 
 # Generate new SSH key pair for using Git
-ssh-keygen -t ed25519 -C "example@mail.com" -q -N ""
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -q -N ""
 
 # Copy public key
-HOST_PUBLIC_KEY=`cat ~/.ssh/id_ed25519.pub`
+VPS_PUBLIC_KEY=`cat ~/.ssh/id_ed25519.pub`
 # Upload public key to Github
 curl \
-    -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
-    --data '{"title":"Vultr VPS","key":"'"$HOST_PUBLIC_KEY"'"}' \
+    -H "Authorization: token $GH_ACCESS_TOKEN" \
+    --data '{"title":"Vultr VPS","key":"'"$VPS_PUBLIC_KEY"'"}' \
     https://api.github.com/user/keys
 
 
 GITHUB_USERNAME=""
 # Write host to config file
 echo """
-Host github.com-$GITHUB_USERNAME
+Host github.com-$GH_USERNAME
  HostName github.com
  User git
  IdentityFile ~/.ssh/id_ed25519
@@ -52,10 +59,7 @@ Host github.com-$GITHUB_USERNAME
 ROOT_DIR="/home/docker/"
 # WORK_DIR=$ROOT_DIR
 
-# SSH URL for your repository
-GITHUB_SSH_URL=""
-
 # Change working directory
 cd $ROOT_DIR
 
-git clone $GITHUB_SSH_URL
+git clone $GH_SSH_URL
