1aaab3ad19a2e8e56259aca79f47a31d0fe53253
Sam Kerridge <sam.kerridge@xero.com>
2021-08-29 20:59:06 +1200

    If wrapped ssh actions to check for existence of previous key

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
 # !/bin/bash
 
-echo $1
-echo $2
-echo $3
-echo $4
-
-export VULTR_SSH_PUBLIC_KEY = <VULTR_SSH_PUBLIC_KEY>
-
-export VULTR_API_KEY=<VULTR_API_KEY>
+if [ ! -z "$(vultr-cli instance list | grep django)" ]
+then
+    vultr-cli instance list
+fi
